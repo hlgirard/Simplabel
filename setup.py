482a3998255e80db98e2e29de2ac9be4ae6b0aec
hlgirard <hl.girard@gmail.com>
2019-01-24 16:03:06 -0500

    Added a binary for command line use and setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='simplabel',
+      version='0.0.1',
+      description='Simple tool to manually label images in disctinct categories to build training datasets.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/hlgirard/Simplabel',
+      author='Henri-Louis Girard',
+      author_email='hl.girard@gmail.com',
+      license='GPLv3',
+      packages=find_packages(),
+      install_requires=[
+          'pillow',
+      ],
+      scripts=[
+          'bin/simplabel'
+      ],
+      zip_safe=False)
