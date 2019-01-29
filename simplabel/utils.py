827ff4dc6afe5be26d3a34a722c1b5574670e820
hlgirard <hl.girard@gmail.com>
2019-01-29 14:35:51 -0500

    Implemented a function to move labelled file to separate directories

--- simplabel/utils.py
+++ simplabel/utils.py
@@ -1,6 +1,32 @@
+import os
+import pickle
+import shutil
+
+def move_to_dict(rawDirectory, labelledDirectory, labelled_dict):
+    # Get all categories that exist in the dictionary
+    categories = set(labelled_dict.values())
+    # Check existence of main directory
+    if not os.path.exists(labelledDirectory):
+        os.mkdir(labelledDirectory)
+    # Check existence of sub folders, create if necessary
+    for label in categories:
+        labelDirect = labelledDirectory + '/' + label
+        if not os.path.exists(labelDirect):
+            os.mkdir(labelDirect)
+    # For each file in dictionary, move it to corresponding directory
+    for image, label in labelled_dict.items():
+        labelDirect = labelledDirectory + '/' + label
+        print("Copying {} to {}".format(image, labelDirect))
+        shutil.copy2(rawDirectory + '/' + image, labelDirect)
+
 
 
 if __name__ == "__main__":
     rawDirectory = 'data/raw'
-    labeledDirectory = 'data/labelled'
-    labels = rawDirectory + '/labels.pkl'
+    labelledDirectory = 'data/labelled'
+    dictPath = rawDirectory + '/labelled.pkl'
+
+    with open(dictPath,'rb') as f:
+        labelled_dict = pickle.load(f)
+
+    move_to_dict(rawDirectory, labelledDirectory, labelled_dict)
