ba02d68a5a10d10b07e8fb169435d041d9208019
hlgirard <hlgirard@users.noreply.github.com>
2019-05-03 17:19:36 -0400

    Reduced default window size.

--- simplabel/simplabel.py
+++ simplabel/simplabel.py
@@ -69,7 +69,7 @@ class ImageClassifier(tk.Frame):
         self.colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
         # Window Dimensions
-        self.winwidth = 1000
+        self.winwidth = 800
         self.imwidth = self.winwidth - 10
         self.imheight = int(self.imwidth // 1.5)
         self.root.geometry("{}x{}".format(self.winwidth, self.imheight+80))
